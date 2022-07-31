a92373206b323fd9dcd60278262384406f2dac3b
Oleh Okarynskyi <okarynskyy@gmail.com>
2022-07-31 17:19:54 +0300

    start

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,7 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
