756349389a944c049934b89b0bca1d64a130aeec
okarynskyi <97628955+okarynskyi@users.noreply.github.com>
2022-07-31 14:20:31 +0000

    Deploying to gh-pages from @ okarynskyi/goit-js-hw-09@a92373206b323fd9dcd60278262384406f2dac3b 🚀

---
+++ 01-color-switcher.78fddc3d.js
@@ -0,0 +1,2 @@
+document.querySelector("button[data-start]"),document.querySelector("button[data-stop]");
+//# sourceMappingURL=01-color-switcher.78fddc3d.js.map
---
+++ 01-color-switcher.b5c4877f.js
@@ -0,0 +1,2 @@
+document.querySelector("button[data-start]"),document.querySelector("button[data-stop]");
+//# sourceMappingURL=01-color-switcher.b5c4877f.js.map
