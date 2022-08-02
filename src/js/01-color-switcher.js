7e4ef068a4cb70d8ff9896aaeeacffebe1cf37fd
Oleh Okarynskyi <okarynskyy@gmail.com>
2022-08-02 22:32:04 +0300

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,6 +1,26 @@
 const startBtn = document.querySelector('button[data-start]');
 const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body')
+let timerId = null;
 
+stopBtn.setAttribute('disabled', true);
+
+startBtn.addEventListener('click', colorFone);
+stopBtn.addEventListener('click', colorStop);
+
+function colorFone() {
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.setAttribute('disabled', true);
+  stopBtn.removeAttribute('disabled');
+}
+
+function colorStop() {
+  clearInterval(timerId);
+  startBtn.removeAttribute('disabled');
+  stopBtn.setAttribute('disabled', true);
+}
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
