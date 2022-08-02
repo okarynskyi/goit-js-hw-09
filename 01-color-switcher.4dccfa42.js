1ee83f42d63f9f6e5a4e1d820bc2d2b99882ca4a
okarynskyi <97628955+okarynskyi@users.noreply.github.com>
2022-08-02 19:32:35 +0000

    Deploying to gh-pages from @ okarynskyi/goit-js-hw-09@7e4ef068a4cb70d8ff9896aaeeacffebe1cf37fd 🚀

---
+++ 01-color-switcher.4dccfa42.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");let d=null;e.setAttribute("disabled",!0),t.addEventListener("click",(function(){d=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.setAttribute("disabled",!0),e.removeAttribute("disabled")})),e.addEventListener("click",(function(){clearInterval(d),t.removeAttribute("disabled"),e.setAttribute("disabled",!0)}));
+//# sourceMappingURL=01-color-switcher.4dccfa42.js.map
--- 01-color-switcher.b5c4877f.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("button[data-start]"),document.querySelector("button[data-stop]");
-//# sourceMappingURL=01-color-switcher.b5c4877f.js.map
